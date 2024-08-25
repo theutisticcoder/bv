66ef3fb6d64da856db58fa06efa6ba6156bb426a
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-08-25 06:15:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const io = require('socket.io')(3000);
+var exec = require('child_process').execFile;
+
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    // Emit an event to the client
+    socket.emit('load-player');    
+    socket.on('x', coo=> {
+        socket.broadcast.emit("playmovex", coo);
+    });
+     socket.on('y', coo=> {
+        socket.broadcast.emit("playmovey", coo);
+    });
+
+    // Handle events from the client
+    
+});
