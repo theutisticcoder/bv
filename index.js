cef3a9c20f537aebcc1150ef22addb498cbbc715
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-08-27 05:06:41 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const io = require('socket.io')(3000);
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+server.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
 var exec = require('child_process').execFile;
 
 
@@ -8,12 +17,12 @@ io.on('connection', (socket) => {
     // Emit an event to the client
     socket.emit('load-player');    
     socket.on('x', coo=> {
-        console.log(coo);
+    console.log(coo);
         socket.broadcast.emit("playmovex", coo);
     });
      socket.on('y', coo=> {
-                console.log(coo);
-        socket.broadcast.emit("playmovey", coo);
+            console.log(coo);
+            socket.broadcast.emit("playmovey", coo);
     });
 
     // Handle events from the client
