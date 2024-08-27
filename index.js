14058262a029efbf50306f58ad138675d5d3547c
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-08-26 21:12:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ io.on('connection', (socket) => {
     // Emit an event to the client
     socket.emit('load-player');    
     socket.on('x', coo=> {
+        console.log(coo);
         socket.broadcast.emit("playmovex", coo);
     });
      socket.on('y', coo=> {
+                console.log(coo);
         socket.broadcast.emit("playmovey", coo);
     });
 
